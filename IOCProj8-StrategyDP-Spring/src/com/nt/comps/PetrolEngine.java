24e2d614871076c78863e4c7c9b42c800d462817
SAITEJ <SAITEJ@SAITEJ-88>
2020-12-21 23:12:19 +0530

    StrategyDP-Spring

---
+++ IOCProj8-StrategyDP-Spring/src/com/nt/comps/PetrolEngine.java
@@ -0,0 +1,18 @@
+package com.nt.comps;
+
+public class PetrolEngine implements IEngine {
+
+	@Override
+	public void start() {
+		System.out.println("Petrol engine is started");
+
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		System.out.println("Petrol engine is stopped");
+
+	}
+
+}

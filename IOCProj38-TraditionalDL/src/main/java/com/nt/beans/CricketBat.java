1f2f69a007c6a53762a04c8b05e7f0cbb7138c38
SAITEJ <SAITEJ@SAITEJ-88>
2021-02-14 20:42:20 +0530

    Traditional DependencyLookup

---
+++ IOCProj38-TraditionalDL/src/main/java/com/nt/beans/CricketBat.java
@@ -0,0 +1,17 @@
+package com.nt.beans;
+
+import java.util.Random;
+
+public class CricketBat {
+	
+	
+	public CricketBat() {
+       System.out.println("CricketBat 0-param Constructor");
+	}
+
+	public int scoreRuns() {
+      System.out.println("CricketBat.scoreRuns()");
+		return new Random().nextInt(100);
+	}
+
+}

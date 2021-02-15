5b57014bf27f1895bfbd3088adc5814b3ecef265
SAITEJ <SAITEJ@SAITEJ-88>
2021-02-15 17:10:17 +0530

    AwareInjection-usings Annotations

---
+++ IOCProj40-Anno-AwareInjection/src/main/java/com/nt/beans/Cricketer.java
@@ -0,0 +1,68 @@
+package com.nt.beans;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+@Component("cricketer")
+
+@PropertySource(value="com/nt/commons/inputs.properties")
+public class Cricketer implements ApplicationContextAware {
+	@Value("${cktr.name}")
+	private String name;
+	@Value("${cktr.jerseyNo}")
+	private int jerseyNo;
+	@Value("${cktrbat.id}")
+	private String beanId;// to just CricketBat obj bean id
+	private ApplicationContext ctx;
+
+	public Cricketer() {
+		System.out.println("Cricketer 0-param Constructor");
+	}
+
+	public void setName(String name) {
+		System.out.println("Cricketer.setName()");
+		this.name = name;
+	}
+
+	public void setJerseyNo(int jeseryNo) {
+		System.out.println("Cricketer.setJeseryNo()");
+		this.jerseyNo = jeseryNo;
+	}
+
+	public void setBeanId(String beanId) {
+		System.out.println("Cricketer.setBeanId()");
+		this.beanId = beanId;
+	}
+	// It is the method of ApplicationContextAware injection not a setter method
+	@Override
+	public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+       System.out.println("Cricketer.setApplicationContext()"); 
+       this.ctx=ctx;
+	}
+	public void bowling() {
+		System.out.println("Cricketer.bowling()");
+		System.out.println("Mr." + name + " with having jerseyNo " + jerseyNo + " is bowling");
+	}
+
+	public void feilding() {
+		System.out.println("Cricketer.feilding()");
+		System.out.println("Mr." + name + " with having jerseyNo " + jerseyNo + " is feilding");
+	}
+
+	public void batting() {
+		System.out.println("Mr." + name + "  with having jerseyNo " + jerseyNo + " is batting");
+		// DependencyLookup to get CricketBat object only in the batting
+		// create an extra IOC container
+		CricketBat bat = ctx.getBean(beanId, CricketBat.class);
+		// use CricketBat object
+		int score = bat.scoreRuns();
+		System.out.println("Cricket runs are  " + score);
+
+	}
+
+
+}
